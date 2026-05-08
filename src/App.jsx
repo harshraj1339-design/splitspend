fb4db4a58ccfc4527225e6bbe180e69a39fba153
Harsh Raj <harshraj1339@gmail.com>
2026-05-08 20:20:13 +0530

    Removed login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
-import { useState } from "react";
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-  const [user, setUser] = useState(
-    localStorage.getItem("user")
-  );
-
-  return (
-    <>
-      {user ? (
-        <Dashboard />
-      ) : (
-        <Login setUser={setUser} />
-      )}
-    </>
-  );
+  return <Dashboard />;
 }
